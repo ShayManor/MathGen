e763d3ae498e7ad88f51bc703a17843d3c24c25c
shay <shay@sentra.io>
2024-11-29 11:13:42 -0800

    Revert "Added effects"
    This reverts commit f856a2eaa21367f9a634e5977f963be321b9da18.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 from Api.export_file import get_in_bucket
-from Movie_Creator.solver import solver
+from Movie_Creator.solver import solver  # Adjust the import as per your project structure
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -26,9 +26,9 @@ def solve():
     if not problem:
         return jsonify({'error': 'No problem provided'}), 400
 
-    result = solver(problem).upload(problem)
+    result = solver(problem)
     print(f'Total time E2E: {time.time()-start_time} seconds')
-    return result
+    return result.upload(problem)
 
 
 @app.route('/ping/uuid_str', methods=['GET'])
